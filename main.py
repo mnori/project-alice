822431339f15647b5260a52473adc3e1e906cb47
Matthew Norris <matthew.gs.norris@gmail.com>
2024-10-11 22:34:09 +0100

    Progress

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import numpy
 import cv2
+from PIL import Image
 
 print("Hello World")
 
+# Make image using set width and height
 width = 1800
 height = 2400
-
 blank_image = numpy.zeros((height, width, 3), numpy.uint8)
 
-cv2.imshow("A New Image", blank_image)
-cv2.waitKey(0)
+# Turn the whole image red
+blank_image[:] = (255, 0, 0)
 
+# Show image object class name
+print(blank_image.__class__.__name__) 
 
-# Create a 1800 x 2400 pixel image using ImageMagick
+# Define the file name to save the image
+image_file_name = 'output/firstone.png'
 
-# Fill it with a solid color
+# Convert the NumPy array to an image object
+image = Image.fromarray(blank_image)
 
-# Add some text to the image
-
+# Save the image object to a PNG file
+image.save(image_file_name)
