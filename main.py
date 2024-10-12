587be45bd50b89a2eb027631386d3262ecd4e896
Matthew Norris <matthew.gs.norris@gmail.com>
2024-10-12 04:23:52 +0100

    Getting there

--- main.py
+++ main.py
@@ -1,46 +1,82 @@
-import numpy
 import cv2
+import numpy as np
 import textwrap
+import PIL
 from PIL import Image
 from matplotlib import pyplot as plt
+from PIL import ImageFont
+from PIL import ImageDraw
 
-def write_text(whole_text):
+def apply_text(whole_text):
 
-    ft2 = cv::freetype::createFreeType2();
-    ft2->loadFontData( "fonts/Ubuntu-Regular.ttf", 0 );
+    print("Hello World")
 
-    wordwrap_width = 36
-    wrapped_text = textwrap.wrap(whole_text, wordwrap_width)
+    font_path = "fonts/Ubuntu-Regular.ttf"
+    font_size = 24
 
-    line_num = 0
-    for line in wrapped_text:
-        text_size = 2.5
-        thickness = 3
-        margin_left = 50
-        margin_top = 150
-        line_height = 120
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        coord = (margin_left + 10, margin_top + (line_num * line_height))
+    font = ImageFont.truetype(font_path, font_size)
+    # font_height = font.getsize("A")[1]  # Calculate the height using a sample character
+    font_height = 24
+    image = np.zeros((1800, 2400, 3), dtype=np.uint8)  # Example image
+    image[:] = (255, 0, 0) 
 
-        ft2->putText(label_image, line,coord, cv2.FONT_HERSHEY_COMPLEX, text_size, (0,0,0), 16, cv2.LINE_AA)
-        ft2->putText(label_image, line,coord, cv2.FONT_HERSHEY_COMPLEX, text_size, (255,255,255), 4, cv2.LINE_AA)
+    text = "Hello, World!"
+    color = (255, 255, 255)  # White color
+    position = (50, 50)  # Position to draw the text
 
-        # cv2.putText(label_image, line, coord, font, text_size, (0, 255, 0), thickness, cv2.LINE_AA)
-        line_num += 1
+    image_pil = Image.fromarray(image)  # Convert OpenCV image to PIL image
+    draw = ImageDraw.Draw(image_pil)
+    draw.text(position, text, font=font, fill=color)
+    image_with_text = np.array(image_pil)  # Convert PIL image back to OpenCV image
+
+    # cv2.imshow("Image with Text", image_with_text)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # Define the file name to save the image
+    image_file_name = 'output/firstone.png'
+
+    # Convert the NumPy array to an image object
+    image_with_text = Image.fromarray(image_with_text)
+
+    # Save the image object to a PNG file
+    image_with_text.save(image_file_name)
+
+    # ft = cv2.freetype.createFreeType2()
+    # ft.loadFontData("fonts/Ubuntu-Regular.ttf", 0)
+    # width, height = ft.getTextSize(text, font_height, thickness)[0]
+
+    # wordwrap_width = 36
+    # wrapped_text = textwrap.wrap(whole_text, wordwrap_width)
+
+    # line_num = 0
+    # for line in wrapped_text:
+    #     text_size = 2.5
+    #     thickness = 3
+    #     margin_left = 50
+    #     margin_top = 150
+    #     line_height = 120
+    #     font = cv2.FONT_HERSHEY_SIMPLEX
+    #     coord = (margin_left + 10, margin_top + (line_num * line_height))
+
+    #     cv2.putText(label_image, line,coord, cv2.FONT_HERSHEY_COMPLEX, text_size, (0,0,0), 16, cv2.LINE_AA)
+    #     cv2.putText(label_image, line,coord, cv2.FONT_HERSHEY_COMPLEX, text_size, (255,255,255), 4, cv2.LINE_AA)
+
+    #     # cv2.putText(label_image, line, coord, font, text_size, (0, 255, 0), thickness, cv2.LINE_AA)
+    #     line_num += 1
 
 
-print("Hello World")
 
-# Make image using set width and height
-width = 1800
-height = 2400
-label_image = numpy.zeros([height, width, 3], dtype=numpy.uint8)
+# # Make image using set width and height
+# width = 1800
+# height = 2400
+# label_image = numpy.zeros([height, width, 3], dtype=numpy.uint8)
 
-# Turn the whole image red
-# It's RGB when it gets saved as a file, but BGR displayed in a window using cv2.imshow
-label_image[:] = (255, 0, 0) 
+# # Turn the whole image red
+# # It's RGB when it gets saved as a file, but BGR displayed in a window using cv2.imshow
+# label_image[:] = (255, 0, 0) 
 
-whole_text = 'You are wondering what on earth is this thing? Let us explain. '
+whole_text = 'You are wondering: "what on earth is this thing?" Let us explain. '
 whole_text += 'Important information can be found within this device, information that requires a trigger warning. '
 whole_text += 'We are bringing this information to you in peace to warn you about a threat in the local area. '
 whole_text += 'This device will not harm your computer. '
@@ -48,8 +84,8 @@ whole_text += 'Along with the main cache of information within, we have included
 whole_text += 'We invite you to consider the information and evidence we are providing as it will help protect you and and your loved ones. '
 whole_text += 'Stay safe. '
 
-write_text(whole_text)
-# print(wrapped_text.__class__.__name__)
+apply_text(whole_text)
+# # print(wrapped_text.__class__.__name__)
 
 
 
@@ -84,26 +120,18 @@ write_text(whole_text)
 # fontScale = 1
  
 
-# Line thickness of 2 px
-thickness = 2
+# # Line thickness of 2 px
+# thickness = 2
  
-# Using cv2.putText() method
-# label_image = cv2.putText(label_image, 'OpenCV', org, font, 
-#                    fontScale, color, thickness, cv2.LINE_AA)
+# # Using cv2.putText() method
+# # label_image = cv2.putText(label_image, 'OpenCV', org, font, 
+# #                    fontScale, color, thickness, cv2.LINE_AA)
 
-# window_name = 'Label Image'
-# cv2.imshow(window_name, label_image) 
-# cv2.waitKey(0)
+# # window_name = 'Label Image'
+# # cv2.imshow(window_name, label_image) 
+# # cv2.waitKey(0)
 
-# plt.show()
+# # plt.show()
 
-# Define the file name to save the image
-image_file_name = 'output/firstone.png'
-
-# Convert the NumPy array to an image object
-label_image = Image.fromarray(label_image)
-
-# Save the image object to a PNG file
-label_image.save(image_file_name)
 
 
