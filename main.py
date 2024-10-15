0925337600deb636779ae1169b8648fb146f324c
Matthew Norris <matthew.gs.norris@gmail.com>
2024-10-15 19:05:06 +0100

    More refactoring

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ config = {
     "margin_left": 90,
     "margin_top": 50,
     "font_size": font_size,
+    "line_height": 1.3 * font_size,
     "stroke_width": 15,
     "wordwrap_width": 34 / (font_size / 100),
     "background_color": (255, 0, 0),
     "stroke_darkness": 0.5,
     "text_color": (255, 255, 255),  # White color
-    "font_path": "fonts/Ubuntu-Regular.ttf",
+    "font_path": "fonts/Ubuntu-Bold.ttf",
     "img_width": 1800,
     "img_height": 2400,
     "image_file_name": 'output/firstone.png'
@@ -34,15 +35,14 @@ def apply_text(whole_text):
         int(config["background_color"][2] * config["stroke_darkness"]))
     wrapped_text = textwrap.wrap(whole_text, config["wordwrap_width"])
     font = ImageFont.truetype(config["font_path"], config["font_size"])
-    image = np.zeros((config["img_height"], config["img_width"], 3), dtype=np.uint8)  # Example image
+    image = np.zeros((config["img_height"], config["img_width"], 3), dtype=np.uint8)
     image[:] = config["background_color"]
-    image_pil = Image.fromarray(image)  # Convert OpenCV image to PIL image
+    image_pil = Image.fromarray(image)
     draw = ImageDraw.Draw(image_pil)
 
     line_num = 0
     for line in wrapped_text:
-        line_height = config["font_size"]
-        coord = (config["margin_left"], config["margin_top"] + (line_num * line_height))
+        coord = (config["margin_left"], config["margin_top"] + (line_num * config["line_height"]))
         draw.text(
             coord, 
             line, 
@@ -52,12 +52,8 @@ def apply_text(whole_text):
             stroke_fill=stroke_fill)
         line_num += 1
 
-    image_with_text = np.array(image_pil)  # Convert PIL image back to OpenCV image
-
-    # Convert the NumPy array to an image object
+    image_with_text = np.array(image_pil)  #
     image_with_text = Image.fromarray(image_with_text)
-
-    # Save the image object to a PNG file
     image_with_text.save(config["image_file_name"])
 
 main()
