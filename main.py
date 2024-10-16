633ca786036693e68e6c5a7e6c7980e084f6ff9e
Matthew Norris <matthew.gs.norris@gmail.com>
2024-10-16 08:02:17 +0100

    Pasting logo onto canvas

--- main.py
+++ main.py
@@ -3,43 +3,55 @@ import numpy as np
 import textwrap
 import PIL
 from PIL import Image
-from matplotlib import pyplot as plt
 from PIL import ImageFont
 from PIL import ImageDraw
 
-font_size =  140
+font_size =  70
 config = {
-    "margin_left": 90,
-    "margin_top": 50,
-    "font_size": font_size,
-    "line_height": 1.3 * font_size,
-    "stroke_width": 15,
-    "wordwrap_width": 34 / (font_size / 100),
-    "background_color": (255, 0, 0),
-    "stroke_darkness": 0.5,
-    "text_color": (255, 255, 255),  # White color
+    "img_width": 900,
+    "img_height": 1200,
     "font_path": "fonts/Ubuntu-Bold.ttf",
-    "img_width": 1800,
-    "img_height": 2400,
-    "image_file_name": 'output/firstone.png'
+    "image_file_name": 'output/firstone.png',
+    "background_color": (255, 0, 0),
+    "text_color": (255, 255, 255),  # White color
+    "margin_left": 45,
+    "margin_top": 25,
+    "font_size": font_size,
+    "wordwrap_width": 25,
+    "line_height": 1.3 * font_size,
+    "stroke_width": 8,
+    "stroke_darkness": 0.5
 }
 
 def main():
-    whole_text = "! Important information     ! Trigger warning            ! Stay safe "
-    apply_text(whole_text)
+    create_image()
 
-def apply_text(whole_text):
+def create_image():
+    image = np.zeros((config["img_height"], config["img_width"], 3), dtype=np.uint8)
+    image[:] = config["background_color"]
+    main_image = Image.fromarray(image)
+    draw = ImageDraw.Draw(main_image)
+
+    draw_text(draw)
+    draw_logo(main_image)
+
+    image_with_text = np.array(main_image)
+    image_with_text = Image.fromarray(image_with_text)
+    image_with_text.save(config["image_file_name"])
+
+def draw_text(draw):
+    whole_text = \
+        "! Important information     "+\
+        "! Trigger warning           "+\
+        "! Stay safe "
+
     stroke_fill = (
         int(config["background_color"][0] * config["stroke_darkness"]), 
         int(config["background_color"][1] * config["stroke_darkness"]), 
         int(config["background_color"][2] * config["stroke_darkness"]))
+
     wrapped_text = textwrap.wrap(whole_text, config["wordwrap_width"])
     font = ImageFont.truetype(config["font_path"], config["font_size"])
-    image = np.zeros((config["img_height"], config["img_width"], 3), dtype=np.uint8)
-    image[:] = config["background_color"]
-    image_pil = Image.fromarray(image)
-    draw = ImageDraw.Draw(image_pil)
-
     line_num = 0
     for line in wrapped_text:
         coord = (config["margin_left"], config["margin_top"] + (line_num * config["line_height"]))
@@ -52,8 +64,9 @@ def apply_text(whole_text):
             stroke_fill=stroke_fill)
         line_num += 1
 
-    image_with_text = np.array(image_pil)  #
-    image_with_text = Image.fromarray(image_with_text)
-    image_with_text.save(config["image_file_name"])
+def draw_logo(draw):
+    img = Image.open("assets/anonymous_emblem.png")
+    img = img.resize((200, 200))
+    draw.paste(img, (config["img_width"] - 200, config["img_height"] - 200), img)
 
 main()
